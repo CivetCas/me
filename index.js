0c3bd4e52ce5758ec5baedda6aeba6c496bd44e5
CivetCas <102417097+CivetCas@users.noreply.github.com>
2024-01-02 21:47:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,29 @@ function myFunction() {
     filter = input.value.toUpperCase();
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
-  
     // 循环表格每一行，查找匹配项
     for (i = 0; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[0];
       if (td) {
         if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
           tr[i].style.display = "";
+          hidden();
         } else {
           tr[i].style.display = "none";
+          show();
         }
       } 
     }
   }
-  
+  function show(){
+
+    var myTable= document.getElementById("myTable"); myTable.style.display="block";
+   
+   }
+   
+   function hidden(){
+   
+   var myTable= document.getElementById("myTable"); myTable.style.display ="none";
+   
+   }
+   
